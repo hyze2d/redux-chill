0cae15347e0419eb7966f53a2c8ae9a60dcdad48
starvinmelon <starvinmelon@gmail.com>
2019-01-19 19:01:00 +0200

    init package

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,3 @@ export * from './saga';
 export * from './reducer';
 export * from './actions';
 
-// "history": "^4.7.2",
-//     "immer": "^1.10.5",
-//     "inversify": "^5.0.1",
-//     "redux": "^4.0.1",
-//     "redux-saga": "^0.16.2"
